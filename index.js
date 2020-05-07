87ec57c092f6ec679bb1acbd820b99e171fd546c
prawinn555 <prawinn555@gmail.com>
2020-05-07 17:05:04 +0200

    1 file modified

--- index.js
+++ index.js
@@ -1,26 +1,8 @@
-import React, { Component } from 'react';
-import { render } from 'react-dom';
-import Hello from './Hello';
-import './style.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./src/App";
+import './src/index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'glyphicons-only-bootstrap/css/bootstrap.min.css';
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      name: 'React'
-    };
-  }
-
-  render() {
-    return (
-      <div>
-        <Hello name={this.state.name} />
-        <p>
-          Start editing to see some magic happen :)
-        </p>
-      </div>
-    );
-  }
-}
-
-render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
