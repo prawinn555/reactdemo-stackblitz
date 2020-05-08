6cbc7254803a44891452990229aca3efc413616d
lapin <lapin@psriplakich-Pc.npif.local>
2020-05-08 16:07:19 +0200

    icons

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'glyphicons-only-bootstrap/css/bootstrap.min.css';
+//import 'glyphicons-only-bootstrap/css/bootstrap.min.css';
 
 ReactDOM.render(<App />, document.getElementById("root"));
